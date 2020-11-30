2f486e043f92f5b48bf6f0fdb073dfb6348b378a
anirudh3001 <46090279+anirudh3001@users.noreply.github.com>
2020-11-30 13:38:58 +0530

    SEMESTER_PRACTICALS

--- SEMESTER_PRACTICALS/SEM_MAT_ADD.asm
+++ SEMESTER_PRACTICALS/SEM_MAT_ADD.asm
@@ -1,1 +1,44 @@
+assume cs:code, ds:data
 
+data segment 
+	row1 db 02h
+	row2 db 02h
+	col1 db 02h 
+	col2 db 02h
+	org 0010h
+	matrix1 db 00h, 11h, 22h, 33h 
+	org 0020h
+	matrix2 db 77h, 66h, 55h, 44h
+	org 0030h
+	result db 4 DUP(0)
+data ends
+
+code segment 
+	org 0100h
+start: mov ax,data
+		mov ds, ax
+		mov cl, row1
+		mov dl, row2 
+		cmp cl, dl
+		jne last
+		mov cl, col1
+		mov dl, col2
+		cmp cl, dl
+		jne last
+		mov al,row2
+		mul cl
+		mov cx,ax
+		mov si, offset matrix1
+		mov di, offset matrix2
+		mov bx, offset result
+here: 	mov al, [si]
+		add al, [di]
+		mov [bx], al
+		inc si
+		inc di
+		inc bx
+		loop here
+last: 	mov ah, 4ch
+		int 21h
+code ends
+end start
